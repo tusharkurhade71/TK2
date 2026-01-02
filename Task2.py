e8720293b51e389fe3f8a137c0ff0dad66bf54a0
tusharkurhade71 <tushar.kurhade71@gmail.com>
2026-01-02 22:09:03 +0530

    Add files via upload

--- Task2.py
+++ Task2.py
@@ -1,3 +1,4 @@
-# for i in range(1,51):
-#     print(i)
-print(f"The sum of numbers from 1 to 50 is: {sum(range(1,51))}")
+num = 0
+for i in range(1,51):
+    num = num + i
+print(f"The sum of numbers from 1 to 50 is: {num}")
